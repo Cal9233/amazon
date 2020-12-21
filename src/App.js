a9ae7b75834507935d09fb77e90cfd8b3cd60c04
Cal <calvin.m71@hotmail.com>
2020-12-21 13:40:30 -0800

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Header from "./Header"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <h1>amazon clone</h1>
+    <div className="app">
+    <Header />
+     {/* Home */}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket"
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="header">
+        <img className="header_logo" 
+         src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" 
+         alt="amazon logo"/>
+      <div className="header_search">
+          <input className="header_searchInput" type="text" />
+          <SearchIcon className="header_searchIcon"/>
+      </div>
+      <div className="header_nav">
+        <div className="header_option">
+          <span className="header_option1">
+          Hello
+          </span>
+          <span className="header_option2">
+            Sign In
+          </span>
+        </div>
+        <div className="header_option">
+        <span className="header_option1">
+          Returns
+          </span>
+          <span className="header_option2">
+            Orders
+          </span>
+        </div>
+        <div className="header_option">
+        <span className="header_option1">
+          Your
+          </span>
+          <span className="header_option2">
+           Prime
+          </span>
+        </div>
+        <div className="header_optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header_option2 
+          header_basketCount">0</span>
+        </div>
+      </div>
+
+    </div>
+  
+  )
+}
+
+export default Header
