797bad23ad7da664619b77c6e66adc42f90025d5
Cal <calvin.m71@hotmail.com>
2020-12-21 12:49:54 -0800

    start

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBDv9gRGfbEgw360DBW8mjnFsyxTKgA5pk",
+  authDomain: "fir-dd2ca.firebaseapp.com",
+  projectId: "fir-dd2ca",
+  storageBucket: "fir-dd2ca.appspot.com",
+  messagingSenderId: "464708423149",
+  appId: "1:464708423149:web:205ec6a4ff7e7d42b7abe4",
+  measurementId: "G-4YJL48JW2L"
+};
