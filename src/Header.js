c200149931ba1b5329e81586fd2b912ebfb87c10
Cal <calvin.m71@hotmail.com>
2020-12-22 13:55:30 -0800

    check box numbers added

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import SearchIcon from '@material-ui/icons/Search';
-import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket"
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { Link } from 'react-router-dom';
+import { useStateValue } from "./StateProvider";
 import './Header.css';
+import { ShoppingBasket } from '@material-ui/icons';
 
 const Header = () => {
+
+  const [{ basket }, dispatch ] = useStateValue();
+
   return (
     <div className="header">
-        <img className="header_logo" 
+      <Link to ='/'>
+      <img className="header_logo" 
          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" 
          alt="amazon logo"/>
+      </Link>
+       
       <div className="header_search">
           <input className="header_searchInput" type="text" />
           <SearchIcon className="header_searchIcon"/>
@@ -38,11 +47,13 @@ const Header = () => {
            Prime
           </span>
         </div>
+        <Link to='/checkout'>
         <div className="header_optionBasket">
           <ShoppingBasketIcon />
           <span className="header_option2 
-          header_basketCount">0</span>
+          header_basketCount">{basket.length}</span>
         </div>
+        </Link>
       </div>
 
     </div>
